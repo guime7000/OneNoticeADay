ca797c029db25f8d4618bc6caf9d2ad9780e7cbc
guime7000 <tguimezanes@gmail.com>
2023-04-29 22:50:12 +0200

    App tested - Not ready for production

---
+++ src/App/onenotice/onenoticeapp/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+from .models import Numpy
+
+from random import choice
+
+
+def index(request):
+    # maxFunctions = Numpy.objects.
+    numpyDocUrl = str("https://numpy.org/doc/stable/reference/generated/")
+    myFunc = Numpy.objects.get(id=choice(range(1, 2211, 1)))
+    myFuncName = myFunc.function_name
+    myFuncPath = myFunc.local_path + myFunc.filename
+    officialDocUrl = numpyDocUrl + myFunc.filename
+
+    with open(myFuncPath) as docFile:
+        docToInsert = docFile.read()
+
+    template = loader.get_template("onenoticeapp/index.html")
+    # context = {"myFuncName": myFuncName}
+    context = {
+        "officialDocUrl": officialDocUrl,
+        "funcDocText": mark_safe(docToInsert),
+        "myFuncName": myFuncName,
+    }
+
+    return render(request, "onenoticeapp/index.html", context)
